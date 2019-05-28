9464880fb1305d281da85514c572944185fd59e1
adibangkai <adibangkai@gmail.com>
2019-05-28 09:36:15 +0700

    add routes

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require(`mongoose`);
+const config = require(`config`);
+const db = config.get(`mongoURI`);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+    console.log("connected");
+  } catch (err) {
+    console.error(err.message);
+    //exit process with failure
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get(`/`,(req,res)=> res.send('API RUNNING'));
+//connect DATABASE
+connectDB();
+
+app.get(`/`, (req, res) => res.send("API RUNNING"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`server start on port ${PORT}`));
+app.listen(PORT, () => console.log(`server start on port ${PORT}`));
