64cd3894bbf351ee846300ad89200045f2dc9ffd
adibangkai <adibangkai@gmail.com>
2019-07-08 01:51:04 +0700

    finale

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   );
 
   return (
-    <nav className='navbar bg-dark'>
+    <nav className='navbar bg-primary'>
       <h1>
         <Link to='/'>
           <i className='fas fa-upload' /> Manggung
